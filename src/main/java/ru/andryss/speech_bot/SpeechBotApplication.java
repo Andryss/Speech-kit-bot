428256f6417f767ad2413a2737d93a44bacaa90e
Andryss <andr.krivosheev@gmail.com>
2025-09-20 17:14:22 +0300

    Initial commit

---
+++ src/main/java/ru/andryss/speech_bot/SpeechBotApplication.java
@@ -0,0 +1,13 @@
+package ru.andryss.speech_bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpeechBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpeechBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/andryss/speech_bot/SpeechBotApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.andryss.speech_bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpeechBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
